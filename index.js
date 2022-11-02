fbf4cae9189c43ef75dbcd6839363d024c96853e
Bfunk54 <ben_fein@icloud.com>
2022-11-02 17:35:17 -0400

    express middleware

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
