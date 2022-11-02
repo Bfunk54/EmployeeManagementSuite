a9b210ea3f903951e6e9c1f3aac39613d15360d7
Bfunk54 <ben_fein@icloud.com>
2022-11-02 17:34:38 -0400

    switched code to the correct index js

--- db/index.js
+++ db/index.js
@@ -1,8 +1,1 @@
-// Import and require mysql2, express, and inquirer
-const express = require('express');
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const fs = require('fs');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
