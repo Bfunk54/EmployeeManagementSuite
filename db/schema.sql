a6650150f60824557f165db06465811a0a370b9c
Bfunk54 <ben_fein@icloud.com>
2022-11-02 17:28:32 -0400

    created the first table for department

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS management_db;
 CREATE DATABASE management_db;
 
 USE management_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dep_name VARCHAR(30) NOT NULL
+  REFERENCES roles(department_id)
+);
